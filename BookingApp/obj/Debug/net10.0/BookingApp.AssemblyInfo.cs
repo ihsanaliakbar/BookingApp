be28340efa4344d98eb1ea7d9e30bc1135a60b7f
Ihsan Akbar <ihsan.akbar@neogeo.com>
2026-02-26 11:12:45 +0100

    add EFcore EFcore.design and postgresql nuget package

--- BookingApp/obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
+++ BookingApp/obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7982367f9ce6d2a6c97dabef9cf6ab4d47895724")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

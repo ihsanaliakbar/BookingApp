fb3f797ecb0f22ee7ba53e7d6c561089a4f051bb
ihsanaliakbar <tataeihsan@gmail.com>
2026-03-06 10:39:03 +0100

    update db: rename duration to durationMinutes in service table

--- BookingApp/Migrations/20260306093703_RenameDurationToDurationMinutes.Designer.cs
+++ BookingApp/Migrations/20260306093703_RenameDurationToDurationMinutes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookingApp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookingApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260306093703_RenameDurationToDurationMinutes")]
+    partial class RenameDurationToDurationMinutes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingApp/Migrations/20260306093703_RenameDurationToDurationMinutes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDurationToDurationMinutes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Duration",
+                table: "Services",
+                newName: "DurationMinutes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DurationMinutes",
+                table: "Services",
+                newName: "Duration");
+        }
+    }
+}
